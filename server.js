57d33a15740cf03866c1f5458f0b93fc61901c51
Aidancm1 <128535723+Aidancm1@users.noreply.github.com>
2023-09-05 10:21:04 -0400

    stressing a lot of handlebar stuff

---
+++ server.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const routes = require('./controllers');
+
+const app = express();
+
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Shh Secret Stuff',
+    cookie: {
+        maxAge: 325000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view enginer', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on port 3001'));
+});
